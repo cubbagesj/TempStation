36d1188d33247fe9a14619f42a33456d99b97f5c
Sam Cubbage <cubbagesj20879@gmail.com>
2017-03-10 19:58:04 -0500

    Removed Adafruit IO posting and SenseHat code

--- homeData.py
+++ homeData.py
@@ -3,8 +3,8 @@
 # in the house.  Each module is set to broadcast a UDP message
 # with its readings to 192.168.1.226:21567
 #
-# This script reads the messages and parses the readings, then
-# posts the readings to adafruit IO.  I was using Initial State but 
+# This script used to post to Adafruit IO but stopped for now
+# I was using Initial State but 
 # stopped because of limits on free streams
 #
 # Last update: 
@@ -12,8 +12,6 @@
 #
 
 import socket
-from Adafruit_IO import Client
-from sense_hat import SenseHat
 
 # open a socket to listen for data on
 # all modules send to the same port
@@ -22,13 +20,9 @@ UDPSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 listen_addr = ("", 21567)
 UDPSock.bind(listen_addr)
 
-#Set up the adafruit client
-aio = Client('6a76fbbf0c46e84971cf5529f166c3ae3fde2b1d')
 
 shttempf = shthumidity = dhttempf = dhthumidity = 0.0
 sht2tempf = sht2humidity = Sitempf = Sihumidity = 0.0
-# Setup sense hat
-#sense = SenseHat()
 
 while True:
     data, addr = UDPSock.recvfrom(1024)
@@ -48,11 +42,6 @@ while True:
                 dhttempf = float(message[5])
                 dhthumidity = float(message[6])
 
-            # Now send to adafruit
-            aio.send('InsideTemp', shttempf)
-            aio.send('InsideHum', shthumidity)
-            aio.send('InsideTemp2', dhttempf)
-            aio.send('InsideHum2', dhthumidity)
 
         if message[0] == 'ESP2':
             # ESP2 has a DHT22 and an Si7021 module
@@ -63,16 +52,6 @@ while True:
                 Sitempf = float(message[5]) * (9.0/5.0) + 32.0
                 Sihumidity = float(message[6])
 
-            # Now send to adafruit
-            aio.send('InsideTemp3', sht2tempf)
-            aio.send('InsideHum3', sht2humidity)
-            aio.send('OutsideTemp', Sitempf)
-            aio.send('OutHumid', Sihumidity)
-
-        # Now do senseHat
-        #tempf = sense.get_temperature_from_humidity() * (9.0/5.0) + 32.0
-        #aio.send('InsideTemp3', tempf)
-        #aio.send('InsideHum3', sense.humidity)
 
     except:
         pass
